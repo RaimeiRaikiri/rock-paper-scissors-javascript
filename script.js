07227659edc8fbd47d6229cc8433deacd3856956
Michael S <mikescutts10@gmail.com>
2023-09-29 22:33:21 +0100

    assigned 1-3 to rock..scissors, made it upperacse, had to move var decleration to before the fucntion

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-// create functio called getComputerChoice that creates random number 1-3 
 // create var called computerSelection that takes the random number generated
-// assign 1,2,3 to rock , paper , scissors 
-// change that to uppercase
+let computerSelection;
+// create functio called getComputerChoice that creates random number 1-3 
+function getComputerChoice() {
+    computerSelection = Math.floor(Math.random() * 3) + 1;
+    // assign 1,2,3 to rock , paper , scissors 
+    // change that to uppercase
+    if (computerSelection === 1) {
+        computerSelection = 'ROCK';
+    } else if (computerSelection === 2) {
+        computerSelection = 'PAPPER';
+    } else if (computerSelection === 3) {
+        computerSelection = 'SCISSORS';
+    }
+}
+
 // create var playerSelection
 // create a prompt that takes rock , paper , scissors case insensitive by setting to uppercase 
 // add prompt answer to playerSelection
