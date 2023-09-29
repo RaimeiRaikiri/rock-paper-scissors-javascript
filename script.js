ad24f3c9b36c5fff51b5394970e885da1596b3e8
Michael S <mikescutts10@gmail.com>
2023-09-29 22:43:21 +0100

    added a nested if statement that determines conditions of the game and if you win or lose

--- script.js
+++ script.js
@@ -18,7 +18,26 @@ function getComputerChoice() {
 let playerSelection;
 // create a prompt that takes rock , paper , scissors case insensitive by setting to uppercase 
 // add prompt answer to playerSelection
+playerSelection = prompt('Please declare your choice: Rock, Paper, or Scissors', '');
+playerSelection = playerSelection.toUpperCase();
+
 // write if...else that decides winner and loser
+if (playerSelection === 'ROCK'
+    || playerSelection === 'PAPER'
+    || playerSelection === 'SCISSORS') {
+        if (playerSelection === computerSelection) {
+        console.log('It\'s a draw!')
+        } else if (playerSelection === 'ROCK' && computerSelectionq === 'SCISSORS') {
+            console.log('You Win! Congratulations!')
+        } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
+            console.log('You Win! Congratulations!')
+        } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+            console.log('You Win! Congratulations!')
+        } else {
+            console.log('You Lose! Better luck next time!')
+        }
+
+    }
 // console should log the players selection and computers before logging a winner
 // create function called oneRound that runs a round of the game 
 // create function called game that runs 5 of oneRound using a for loop
