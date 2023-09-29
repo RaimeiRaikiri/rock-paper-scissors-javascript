9d4b4c20734d60198a036b90f781e26a382a32dd
Michael S <mikescutts10@gmail.com>
2023-09-29 22:26:42 +0100

    created pseudocode to help tackle the code

---
+++ script.js
@@ -0,0 +1,12 @@
+// create functio called getComputerChoice that creates random number 1-3 
+// create var called computerSelection that takes the random number generated
+// assign 1,2,3 to rock , paper , scissors 
+// change that to uppercase
+// create var playerSelection
+// create a prompt that takes rock , paper , scissors case insensitive by setting to uppercase 
+// add prompt answer to playerSelection
+// write if...else that decides winner and loser
+// console should log the players selection and computers before logging a winner
+// create function called oneRound that runs a round of the game 
+// create function called game that runs 5 of oneRound using a for loop
+
